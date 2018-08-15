1037eee71a84e02617055a20c687c1e7447ab88b
Linda Lee <redtaq@hotmail.com>
2018-08-15 01:15:04 -0500

    edit

--- client/src/jsxs/AnimalEdit.jsx
+++ client/src/jsxs/AnimalEdit.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Navigation } from './Navigation.jsx'
 
 export class AnimalEdit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      deleteAnimals = []
+    }
+  }
 
   editAnimal = () => {
 
